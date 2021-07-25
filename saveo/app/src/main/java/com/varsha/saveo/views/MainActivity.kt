f941c74d8c347fdf93e4de0c0e2d63c83a92b8dc
masai <varshakumari8999@gmail.com>
2021-07-25 15:51:14 +0530

    LayoutDesigning

--- saveo/app/src/main/java/com/varsha/saveo/views/MainActivity.kt
+++ saveo/app/src/main/java/com/varsha/saveo/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.varsha.saveo
+package com.varsha.saveo.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.varsha.saveo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
